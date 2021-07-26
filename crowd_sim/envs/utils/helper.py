097c59b0f175cd472ab059218d48906baea4d8e1
Evan Tan <tcyevan@gmail.com>
2021-07-26 13:36:11 +0800

    Fixed VelocityRectangle, added smoothing function for progress.csv

--- crowd_sim/envs/utils/helper.py
+++ crowd_sim/envs/utils/helper.py
@@ -1,6 +1,7 @@
 import shapely.geometry
 import numpy as np
 from crowd_sim.envs.utils.agent import Agent
+from typing import List
 
 # wrap angle between [-180, 180]
 def wrap_angle(angle):
@@ -15,10 +16,23 @@ def vec_norm(A, B):
     return np.linalg.norm([A[0] - B[0], A[1] - B[1]])
 
 
+# smoothing function for visualizing noisy training data
+def smooth_data(scalars: List[float], weight: float) -> List[float]:
+    assert weight >= 0 and weight <= 1
+    last = scalars[0]  # First value in the plot (first timestep)
+    smoothed = list()
+    for point in scalars:
+        smoothed_val = last * weight + (1 - weight) * point
+        smoothed.append(smoothed_val)
+        last = smoothed_val
+
+    return smoothed
+
+
 # https://shapely.readthedocs.io/en/stable/manual.html#shapely.geometry.box
 class VelocityRectangle:
-    # how many time steps ahead to extend velocity rectangle by
-    T_STEPS = 1
+    # k=3, how many time steps ahead to extend velocity rectangle by
+    T_STEPS = 3
 
     def __init__(self, agent: Agent):
         self._agent = agent
@@ -26,23 +40,34 @@ class VelocityRectangle:
         self._create_rect()
 
     def _create_rect(self):
+        # in agent.py [self.px, self.py, self.vx, self.vy, self.radius]
+        properties = self._agent.get_observable_state_list()
+        vx, vy = properties[2:4]
         # scaling factor for rectangle length, see Kevin's paper
-        rlength = self.T_STEPS * (self._agent.vx ** 2 + self._agent.vy ** 2) ** 0.5
-        rwidth = 2 * self._agent.radius
-        heading = self._agent.theta
+        radius = properties[4]
+        rwidth = 2 * radius
+        rlength = self.T_STEPS * (vx ** 2 + vy ** 2) ** 0.5
+        # DO NOT USE self._agent.theta as it is never updated
+        # -90deg since origin_rect is oriented at +90deg
+        robot_heading = np.arctan2(vy, vx)
+        box_heading = robot_heading - np.pi / 2
 
-        # create rectangle at 0,0 first, bottom edge touching horizontal axis
+        # create rectangle at 0,0 first, "bottom edge" touching horizontal axis
         origin_rect = shapely.geometry.box(-rwidth / 2, 0, rwidth / 2, rlength)
         # rotate ABOUT (0,0) based on agent heading
-        rot_rect = shapely.affinity.rotate(origin_rect, angle=heading, origin=(0, 0))
+        rot_rect = shapely.affinity.rotate(
+            origin_rect, angle=box_heading, origin=(0, 0), use_radians=True
+        )
         # translate to get final rect
-        self._vec_rect = shapely.affinity.translate(
-            rot_rect, self._agent.px, self._agent.py
+        x_os = radius * np.cos(robot_heading)
+        y_os = radius * np.sin(robot_heading)
+        self._vel_rect = shapely.affinity.translate(
+            rot_rect, self._agent.px + x_os, self._agent.py + y_os
         )
 
     def intersects(self, VelocityRectangleObject):
-        assert hasattr(VelocityRectangleObject, "_vec_rect")
-        return self._vec_rect.intersects(VelocityRectangleObject._vec_rect)
+        assert hasattr(VelocityRectangleObject, "_vel_rect")
+        return self._vel_rect.intersects(VelocityRectangleObject._vel_rect)
 
 
 class SidePreference:
