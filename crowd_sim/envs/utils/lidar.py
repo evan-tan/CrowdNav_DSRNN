8be95b42f5504c445e46f413e612fe600565b66a
Evan Tan <tcyevan@gmail.com>
2021-09-11 00:58:23 +0800

    Update lidar sim

--- crowd_sim/envs/utils/lidar.py
+++ crowd_sim/envs/utils/lidar.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import shapely.wkt
 from crowd_sim.envs.utils.helper import (
+    make_mpl_line,
     make_shapely_ellipse,
     make_shapely_polygon,
     vec_norm,
@@ -25,7 +26,7 @@ class LidarSensor:
         self.MAX_DIST = cfg["max_range"]
         self.ANGLES = np.linspace(0, 360, cfg["num_spacings"] + 1)
 
-        self.obstacles = []
+        self.obstacles = {}
         self.mpl_dict = {}
         self.collision_pts = []  # absolute x,y where beams end
 
@@ -36,12 +37,15 @@ class LidarSensor:
     ):
         if "walls" in mode:
             # create polygon representing walls
-            self.obstacles.append(make_shapely_polygon(obstacle_pts))
+            self.obstacles["walls"] = [make_shapely_polygon(obstacle_pts)]
+
         elif "agents" in mode:
             # assumes a list of lists
             # format: (pos x, pos y, radius)
+            tmp_list = []
             for agent in obstacle_pts:
-                self.obstacles.append(make_shapely_ellipse(agent[-1], agent[:2]))
+                tmp_list.append(make_shapely_ellipse(agent[-1], agent[:2]))
+            self.obstacles["agents"] = tmp_list
         else:
             pass
 
@@ -54,8 +58,12 @@ class LidarSensor:
         self._lidar_spin()
 
     def _lidar_spin(self):
+        """Get all collision points between lidar beam and obstacles"""
+
         mpl_lines = []
-        for _, angle in enumerate(self.ANGLES + self.SENSOR_HEADING):
+        agent_beams = {}  # key: agent_id, value: list of beam_ids
+        for beam_idx, angle in enumerate(self.ANGLES + self.SENSOR_HEADING):
+            # create beams and rotate to correct angle
             line = LineString(
                 [
                     self.SENSOR_POS,
@@ -63,18 +71,22 @@ class LidarSensor:
                 ]
             )
             rot_line = rotate(line, angle, origin=self.SENSOR_POS)
-            # check for any intersects
+
+            all_obstacles = []
+            for _, polygons in self.obstacles.items():
+                all_obstacles += polygons
+            # check for any intersects between current beam and ALL obstacles
             idx = -1
-            for i, obstacle in enumerate(self.obstacles):
+            for i, obstacle in enumerate(all_obstacles):
                 any_intersect = rot_line.intersects(obstacle)
                 if any_intersect:
                     idx = i
-            # get specific points
+
             if idx != -1:
                 # oh my god this is jank...
                 chopped_line = shapely.wkt.loads(
                     shapely.wkt.dumps(
-                        rot_line.intersection(self.obstacles[idx]),
+                        rot_line.intersection(all_obstacles[idx]),
                         rounding_precision=self.NUM_DP,
                     )
                 )
@@ -83,25 +95,34 @@ class LidarSensor:
 
                 if vec_norm(start.coords[0], self.SENSOR_POS) > 0:
                     # multiple intersection points
+                    # if agent, start/end = closer/further parts of polygon
                     x_data = (self.SENSOR_POS[0], start.coords[0][0])
                     y_data = (self.SENSOR_POS[1], start.coords[0][1])
+                    col_pt = start.coords[0]
                 else:
-                    # if only wall, start = self.SENSOR_POS
+                    # if only wall, start = sensor pos, end = point on wall
                     x_data = (start.coords[0][0], end.coords[0][0])
                     y_data = (start.coords[0][1], end.coords[0][1])
-                mpl_lines.append(
-                    mlines.Line2D(
-                        x_data,
-                        y_data,
-                        solid_capstyle="round",
-                        marker="o",
-                        markersize=2,
-                        color="r",
-                    )
-                )
-                col_pt = (x_data[1], y_data[1])
+                    col_pt = end.coords[0]
+                mpl_lines.append(make_mpl_line(x_data, y_data))
                 self.collision_pts.append(col_pt)
+
+                # determine
+                if all_obstacles[idx] in self.obstacles["agents"]:
+                    # get agent id
+                    agent_id = self.obstacles["agents"].index(all_obstacles[idx])
+                    if agent_id not in agent_beams.keys():
+                        # create key val pair
+                        agent_beams[agent_id] = [beam_idx]
+                    else:
+                        # if key exists, append to it
+                        agent_beams[agent_id].append(beam_idx)
         self.mpl_dict["lines"] = mpl_lines
+
+        # print(agent_beam_dupes)
+        # remove duplicates for single agents,
+        for key, val in enumerate(agent_beams):
+            pass
 
     def get_mpl_dict(self):
         return self.mpl_dict
@@ -115,7 +136,7 @@ if __name__ == "__main__":
     ax.set_xlabel("x(m)", fontsize=16)
     ax.set_ylabel("y(m)", fontsize=16)
 
-    cfg = {"max_range": np.sqrt(2) * val / 2, "num_spacings": 145}
+    cfg = {"max_range": np.sqrt(2) * val / 2, "num_spacings": 20}
     lidar = LidarSensor(cfg)
     t = val / 4
     wall_pts = [(-t, -t), (t, -t), (t, t), (-t, t)]
@@ -133,19 +154,21 @@ if __name__ == "__main__":
     for l in lines:
         ax.add_line(l)
 
-    end_pts = lidar.collision_pts
-    if end_pts[0] != end_pts[-1]:
-        end_pts.append(end_pts[0])
-    polygon = patches.Polygon(xy=end_pts, fill=False, color="g")
+    col_pts = lidar.collision_pts
+    if col_pts[0] != col_pts[-1]:
+        col_pts.append(col_pts[0])
+    polygon = patches.Polygon(xy=col_pts, fill=False, color="g")
     ax.add_patch(polygon)
-    ax.axis("on")
+    ax.axis("off")
 
     # extract image data from mpl plot
     fig.canvas.draw()
     data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep="")
     w, h = fig.canvas.get_width_height()
     data = data.reshape(h, w, 3)
-    # from hough import test_opencv, test_skimage
+
+    from hough import test_opencv, test_skimage
+
     # test_skimage(data)
     # test_opencv(data)
     plt.show()
