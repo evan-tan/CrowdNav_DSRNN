3fbb86bc05a3989cf85f6f929ab7562f56903128
Evan Tan <tcyevan@gmail.com>
2021-09-29 15:20:58 +0800

    Update install script

--- setup/full_setup.sh
+++ setup/full_setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
+source $HOME/miniconda3/etc/profile.d/conda.sh
+
 # main folder where all repos will be cloned into
 REPOS_PATH="$HOME/repos/fyp_repos"
 # conda environment name
@@ -27,27 +30,59 @@ fi
 
 # clone all repos
 cd "$REPOS_PATH"
-git clone git@github.com:evan-tan/CrowdNav_DSRNN.git
-git clone https://github.com/sybrenstuvel/Python-RVO2.git
-git clone https://github.com/openai/baselines.git
-git clone https://github.com/ChanganVR/socialforce.git
+if [ -d "$REPOS_PATH/CrowdNav_DSRNN" ]; then
+    echo "$REPOS_PATH/CrowdNav_DSRNN already exists!"
+else
+    git clone git@github.com:evan-tan/CrowdNav_DSRNN.git
+fi
+if [ -d "$REPOS_PATH/Python-RVO2" ]; then
+    echo "$REPOS_PATH/Python-RVO2 already exists!"
+else
+    git clone https://github.com/sybrenstuvel/Python-RVO2.git
+fi
+if [ -d "$REPOS_PATH/baselines" ]; then
+    echo "$REPOS_PATH/baselines already exists!"
+else
+    git clone https://github.com/openai/baselines.git
+fi
+if [ -d "$REPOS_PATH/socialforce" ]; then
+    echo "$REPOS_PATH/socialforce already exists!"
+else
+    git clone https://github.com/ChanganVR/socialforce.git
+fi
 
 sudo apt-get install -y python3-tk
 
 alias pip3="python3 -m pip"
 
 #### Create conda environment ####
-source $HOME/miniconda3/etc/profile.d/conda.sh
 conda create -n $ENV_NAME python=$PYTHON_VER -y &&
     #### ACTIVATE CONDA ENVIRONMENT ####
     conda activate $ENV_NAME
 
+# install cuspatial
+# dependencies [cudatoolkit, cudf, rmm]
+conda install -y -c anaconda cudatoolkit=11.0
+conda install -y -c conda-forge -c rapidsai cudf rmm cudatoolkit=11.0
+conda install -y -c conda-forge -c rapidsai cuspatial
+
 # install rvo2
 cd $REPOS_PATH/Python-RVO2 &&
-    pip3 install cython && # old cython version will break
+    # DO NOT FOLLOW README and install old cython, will break
+    conda install -y -c conda-forge cython &&
     python3 setup.py build &&
     python3 setup.py install &&
     pip3 install -e .
+
+# install everything else for DSRNN
+cd $REPOS_PATH/CrowdNav_DSRNN
+# split install into multiple commands so ...
+# solving environment doesn't take forever
+pip install tensorflow tensorboard
+conda install -y -c conda-forge gym numpy pandas
+conda install -y -c conda-forge matplotlib shapely
+conda install -y -c conda-forge scipy scikit-image
+conda install -y pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch
 
 # install openai baselines
 cd $REPOS_PATH/baselines &&
@@ -55,11 +90,9 @@ cd $REPOS_PATH/baselines &&
     sudo apt-get install cmake libopenmpi-dev python3-dev zlib1g-dev &&
     pip3 install -e .
 
-# install everything else for DSRNN
-cd $REPOS_PATH/CrowdNav_DSRNN &&
-    pip3 install -r requirements.txt &&
-    pip3 install -e .
-
 # # install socialforce (UNUSED)
 # cd $REPOS_PATH/socialforce &&
 #     pip3 install -e '.[test,plot]'
+
+# go back to cwd
+cd $CWD
